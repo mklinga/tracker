5ea63e8458170b5c338db8ef7425fa2304aaf5c5
Markus Klinga <laite@gmx.com>
2014-04-13 12:48:44 +0300

    test call to server

--- client/js/timetracker.js
+++ client/js/timetracker.js
@@ -1,5 +1,9 @@
 $( document ).ready(function() {
 	$("header").html("Hallo");
 
+	$.get("http://localhost:8080/hello/markus", function( data ) {
+		$("main").html(data);
+	});
+
 	console.log("I'm right here!");
 });
