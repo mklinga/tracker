bcbe811433ce37b7c2e54e5342be5d682e3ce868
Markus Klinga <laite@gmx.com>
2014-04-13 12:17:19 +0300

    start defining REST server

---
+++ server/mongo-server.js
@@ -0,0 +1,32 @@
+/*
+ * Mongo-server for timetracker
+ *
+ * Provides a REST API at port ???
+ *
+ * Author: Markus Klinga
+ *
+ */
+
+
+var restify = require("restify");
+
+
+function respond(req, res, next) {
+	var responseText = JSON.stringify("Hello " + req.params.name);
+	res.send(responseText);
+	next();
+}
+
+
+var server = restify.createServer();
+server.get("/hello/:name", respond);
+server.head("/hello/:name", respond);
+
+server.post("/hello", function create(req, res, next) {
+	res.send(201, Math.random().toString(36).substr(3, 8));
+	return next();
+});
+
+server.listen(8080, function () {
+	console.log("%s listening at %s", server.name, server.url);
+});
