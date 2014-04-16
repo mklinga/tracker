7831c9facfcfd8910a6af2c15b2cc733ada0c771
Markus Klinga <laite@gmx.com>
2014-04-16 19:23:48 +0300

    initial save to/query from database functions

--- server/db.js
+++ server/db.js
@@ -8,13 +8,82 @@
 
 var mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost/test');
+/*
+ *
+ * Mongodb schemas
+ *
+ */
 
-var db = mongoose.connection;
-
-db.on('error', console.error.bind(console, "connection error"));
-db.once('open', function() {
-	console.log("A connection to mongo database has been made!");
+var userSchema = mongoose.Schema({
+	id: Number,
+	name: String
 });
 
-console.log("here");
+var timeSchema = mongoose.Schema({
+	id: Number,
+	title: String,
+	type: String,
+});
+
+var historySchema = mongoose.Schema({
+	userId: Number,
+	timerId: Number,
+	begin: Number,
+	end: Number
+});
+
+/* mongoose Models */
+var User, Time, History;
+
+/* Connect to mongo database */
+mongoose.connect('mongodb://localhost/test');
+
+/* Database handle */
+var db = mongoose.connection;
+
+db.once('open', function() {
+	/* Our connection to database is now opened! */
+	historySchema.methods.findAllFromSameUser = function (cb) {
+		console.log(this.userId);
+		return this.model('History').find({ userId: this.userId }, cb);
+	};
+
+	User = mongoose.model('User', userSchema);
+	Time = mongoose.model('Time', timeSchema);
+	History = mongoose.model('History', historySchema);
+
+	getAllHistory(1, function(history) {
+		console.log(history);
+	});
+});
+
+db.on('error', console.error.bind(console, "connection error"));
+
+/*
+ *
+ * Handler functions
+ *
+ */
+
+function getAllHistory(userId, cb) {
+	History.find({ userId: userId }, function(err, histories) {
+		if (err) return console.error(err);
+		cb(histories);
+	});
+
+}
+function addNewHistory(historyItem) {
+	var history = new History({
+		userId: historyItem.userId,
+		timerId: historyItem.timerId,
+		begin: historyItem.begin,
+		end: historyItem.end
+	});
+
+	history.save(function(err, item) {
+		if (err) return console.error(err);
+
+		console.log("Saved a new item to history: ");
+		console.log(history);
+	});
+}
