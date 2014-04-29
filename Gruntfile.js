ca63c781b874a0ab944d539c3af38deef5363529
Markus Klinga <laite@gmx.com>
2014-04-29 19:33:48 +0300

    add grunt with contribs: sass, jst, watch

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		sass: {
+			dist: {
+				options: {
+					style: 'compressed'
+				},
+				files: {
+					'css/main.css': 'scss/main.scss',
+				}
+			}
+		},
+
+		watch: {
+			css: {
+				files: 'scss/*.scss',
+				tasks: ['sass'],
+			}
+		},
+
+		jst: {
+			compile: {
+                /*
+				 * options: {
+				 * 	templateSettings: {
+				 * 		interpolate : /\{\{(.+?)\}\}/g
+				 * 	}
+				 * },
+                 */
+				files: {
+					"client/js/templates.js": ["client/templates/**/*.html"]
+				}
+			}
+		}
+
+		
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-jst');
+
+	grunt.registerTask('default', ['sass', 'watch'] );
+};
